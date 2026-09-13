f107612703dc2925d33e9d15f1ac48f4b9b7f37f
Eric <eric@ericjohannesson.com>
2026-09-13 13:02:54 +0200

    added nix packaging

---
+++ default.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.stdenv.mkDerivation {
+  name = "natural-deduction";
+  version = "0";
+  src = ./.;
+  buildInputs = with pkgs; [
+      ocaml
+      ocamlPackages.findlib
+      ocamlPackages.uuseg
+  ];
+  buildPhase = ''
+    make natural-deduction
+  '';
+  installPhase = ''
+    mkdir -p $out/bin
+    cp natural-deduction $out/bin/
+  '';
+}
